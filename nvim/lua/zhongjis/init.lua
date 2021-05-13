71585a48a684ba841ec7c0ecc82e4d29ae113a57
Zhongjie Shen <zhongjie.shen@observepoint.com>
2021-05-12 18:22:49 -0600

    update nvim settings

---
+++ nvim/lua/zhongjis/init.lua
@@ -0,0 +1,15 @@
+require("zhongjis.telescope"***REMOVED***
+
+P = function(v***REMOVED***
+  print(vim.inspect(v***REMOVED******REMOVED***
+  return v
+end
+
+if pcall(require, 'plenary'***REMOVED*** then
+  RELOAD = require('plenary.reload'***REMOVED***.reload_module
+
+  R = function(name***REMOVED***
+    RELOAD(name***REMOVED***
+    return require(name***REMOVED***
+  end
+end
