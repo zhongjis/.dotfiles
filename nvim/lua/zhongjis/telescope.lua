3d02ba13aadcab6d656e508e389d0220630f944b
zshen <zshen@adobe.com>
2021-11-27 23:46:07 -0700

    Update

--- nvim/lua/zhongjis/telescope.lua
+++ nvim/lua/zhongjis/telescope.lua
@@ -27,3 +27,15 @@ require('telescope'***REMOVED***.setup {
 }
 
 require('telescope'***REMOVED***.load_extension('fzy_native'***REMOVED***
+
+-- customized funcs
+local M = {}
+M.search_dot***REMOVED***les = function(***REMOVED***
+    require("telescope.builtin"***REMOVED***.***REMOVED***nd_***REMOVED***les({
+        prompt_title = "< VimRC >",
+        cwd = vim.env.DOTFILES,
+        hidden = true,
+    }***REMOVED***
+end
+
+return M
