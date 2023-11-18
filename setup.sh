23ebcde8b5efd86281e1f2cf28c1a4b87597c9dc
Zhongjie Shen <zshen@adobe.com>
2023-11-18 11:18:54 -0700

    update .tmux.conf and setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,24 @@
-ln -s ~/Documents/dot***REMOVED***les-awesome/.tmux.conf ~
-ln -s ~/Documents/dot***REMOVED***les-awesome/nvim ~/.con***REMOVED***g
+***REMOVED***!/bin/bash
+ln -s nvim ~/.con***REMOVED***g
+ln -s .zshrc ~
+ln -s .tmux.conf ~
+
+***REMOVED*** Install Tmux
+if brew list tmux &>/dev/null; then
+    echo "tmux is already installed. Update Instead"
+    brew upgrade tmux
+***REMOVED***
+    ***REMOVED*** Install tmux if it's not already installed
+    echo "Installing tmux..."
+    brew install tmux
+***REMOVED***
+
+***REMOVED*** Install TPM
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+if [ -d "$TPM_DIR" ]; then
+    echo "Directory $TPM_DIR already exists. Update instead"
+  ***REMOVED***-C "$TPM_DIR" pull origin master
+***REMOVED***
+    ***REMOVED*** If the directory does not exist, clone the repository
+  ***REMOVED***clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+***REMOVED***
