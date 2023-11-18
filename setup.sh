7b5a7eb4c6168c1275d2ec160537ccbcde93fa46
Zhongjie Shen <zshen@adobe.com>
2023-11-18 11:40:16 -0700

    fixed a regression by wrong symlink setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 ***REMOVED***!/bin/bash
-ln -s nvim ~/.con***REMOVED***g
-ln -s .zshrc ~
-ln -s .tmux.conf ~
+CURRENT_DIR=$(pwd***REMOVED***
+echo "[INFO] Current dir: $CURRENT_DIR"
+ln -sf $CURRENT_DIR/nvim ~/.con***REMOVED***g
+ln -sf $CURRENT_DIR/.zshrc ~
+ln -sf $CURRENT_DIR/.tmux.conf ~
 
 ***REMOVED*** Install Tmux
 if brew list tmux &>/dev/null; then
-    echo "tmux is already installed. Update Instead"
+    echo "[INFO] tmux is already installed. Update Instead"
     brew upgrade tmux
 ***REMOVED***
     ***REMOVED*** Install tmux if it's not already installed
@@ -16,7 +18,7 @@ if brew list tmux &>/dev/null; then
 ***REMOVED*** Install TPM
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 if [ -d "$TPM_DIR" ]; then
-    echo "Directory $TPM_DIR already exists. Update instead"
+    echo "[INFO] Directory $TPM_DIR already exists. Update instead"
   ***REMOVED***-C "$TPM_DIR" pull origin master
 ***REMOVED***
     ***REMOVED*** If the directory does not exist, clone the repository
