3b02c4a218e9605f7151ada44b012a1d63936541
zshen <jasonshen96115@gmail.com>
2023-12-16 22:50:41 -0700

    update vim setup and fix some bugs

--- setup.sh
+++ setup.sh
@@ -2,31 +2,30 @@
 
 # Function to install or update a GitHub repository
 install_or_update_git_repo() {
-    local repo_url="$1"
-    local clone_path="$2"
+	local repo_url="$1"
+	local clone_path="$2"
 
-    if [ -d "$clone_path" ]; then
-        echo "[INFO] Updating repository at $clone_path..."
-        git -C "$clone_path" pull origin master
-    else
-        echo "[INFO] Cloning repository from $repo_url to $clone_path..."
-        git clone "$repo_url" "$clone_path"
-    fi
+	if [ -d "$clone_path" ]; then
+		echo "[INFO] Updating repository at $clone_path..."
+		git -C "$clone_path" pull origin master
+	else
+		echo "[INFO] Cloning repository from $repo_url to $clone_path..."
+		git clone "$repo_url" "$clone_path"
+	fi
 }
-
 
 # Function to install or update a Homebrew package
 install_or_update_brew_package() {
-    local package_name="$1"
+	local package_name="$1"
 
-    echo "[INFO] Checking $package_name..."
-    if brew list $package_name &>/dev/null; then
-        echo "[INFO] $package_name is already installed. Updating..."
-        brew upgrade $package_name
-    else
-        echo "[INFO] Installing $package_name..."
-        brew install $package_name
-    fi
+	echo "[INFO] Checking $package_name..."
+	if brew list $package_name &>/dev/null; then
+		echo "[INFO] $package_name is already installed. Updating..."
+		brew upgrade $package_name
+	else
+		echo "[INFO] Installing $package_name..."
+		brew install $package_name
+	fi
 }
 
 # ---- Set Zsh as Default Shell (if not already set) ----
