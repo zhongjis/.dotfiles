8e0a6f174578965b25adc0acab56381dbd255453
zshen <jasonshen96115@gmail.com>
2023-12-24 16:23:22 -0700

    update vimrc

--- nvim/lua/plugins/solarized-osaka.lua
+++ nvim/lua/plugins/solarized-osaka.lua
@@ -1,6 +1,14 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
 }
