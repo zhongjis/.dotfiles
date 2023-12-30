6903f8eb608f8de1b0343ee2563816df9936ecc9
Zhongjie Shen <zshen@adobe.com>
2023-12-30 00:38:52 -0700

    update script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-stow .
+echo "stow ./run-scripts"
+stow -D "run-scripts"
+stow -d "run-scripts" -t "/usr/local/bin" .
+
+# echo "stow ./settings"
+stow -D "./settings"
+stow -d ./settings -t ~ .
