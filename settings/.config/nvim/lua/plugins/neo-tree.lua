6bb569090afcf3d1847e1d5a142f8bfc0ba36924
Zhongjie Shen <zshen@adobe.com>
2024-01-03 21:47:05 -0700

    update

---
+++ settings/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+      hide_dotfiles = false,
+      filtered_items = {
+        hide_gitignored = true,
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+        },
+      },
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
+}
