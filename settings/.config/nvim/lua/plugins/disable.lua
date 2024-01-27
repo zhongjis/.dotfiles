9865a182c4251c272b9e32fddfd3682c209fa54f
Zhongjie Shen <zshen@adobe.com>
2024-01-27 13:33:54 -0500

    update

--- settings/.config/nvim/lua/plugins/disable.lua
+++ settings/.config/nvim/lua/plugins/disable.lua
@@ -28,6 +28,14 @@ return {
     enabled = false,
   },
   {
+    "folke/persistence.nvim",
+    enabled = false,
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
+  {
     "folke/tokyonight.nvim",
     enabled = false,
   },
