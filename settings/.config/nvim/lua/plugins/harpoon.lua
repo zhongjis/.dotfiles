3fceaf8b30b0042a68bc377a45f25b3aaa6fed55
zshen <jasonshen96115@gmail.com>
2024-02-04 11:48:33 -0700

    update harpoon config

--- settings/.config/nvim/lua/plugins/harpoon.lua
+++ settings/.config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,54 @@
 return {
-  "ThePrimeagen/harpoon",
+  "theprimeagen/harpoon",
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    ---@diagnostic disable-next-line: missing-parameter
+    require("harpoon"):setup()
+  end,
+  keys = {
+    {
+      "<leader>a",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Harpoon add file to list",
+    },
+    {
+      "<leader>h",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Harpoon toggle quick menu",
+    },
+    {
+      "<c-h>",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Harpoon to file 1",
+    },
+    {
+      "<c-j>",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Harpoon to file 2",
+    },
+    {
+      "<c-k>",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Harpoon to file 3",
+    },
+    {
+      "<c-l>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Harpoon to file 4",
+    },
+  },
 }
