752e33c09203aa8aafd27178fd165a779af3e917
zshen <jasonshen96115@gmail.com>
2024-02-04 10:51:33 -0700

    reenable some of the lazyvim built-ins

--- settings/.config/nvim/lua/plugins/disabled.lua
+++ settings/.config/nvim/lua/plugins/disabled.lua
@@ -4,26 +4,6 @@ return {
     enabled = false,
   },
   {
-    "echasnovski/mini.nvim",
-    enabled = false,
-  },
-  {
-    "rcarriga/nvim-notify",
-    enabled = false,
-  },
-  {
-    "stevearc/dressing.nvim",
-    enabled = false,
-  },
-  {
-    "folke/noice.nvim",
-    enabled = false,
-  },
-  {
-    "folke/persistence.nvim",
-    enabled = false,
-  },
-  {
     "folke/flash.nvim",
     enabled = false,
   },
@@ -37,18 +17,6 @@ return {
   },
   {
     "folke/tokyonight.nvim",
-    enabled = false,
-  },
-  {
-    "echasnovski/mini.surround",
-    enabled = false,
-  },
-  {
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
-  {
-    "echasnovski/mini.ai",
     enabled = false,
   },
   {
