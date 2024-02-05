4d684fe3fc4596dae3b22ecada2203287e4cb81d
Zhongjie Shen <zshen@adobe.com>
2024-02-05 10:46:14 -0700

    update

--- settings/.config/nvim/lua/config/lazy.lua
+++ settings/.config/nvim/lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
